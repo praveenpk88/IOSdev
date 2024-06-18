08a445cebff1079804d29785f25ac75e59d468b0
reshma22-dot <reshmananjundangmail.com>
2024-06-19 00:46:51 +0530

    New IOS sample app added.

---
+++ sample app creation/sample app creation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample app creation
+//
+//  Created by Reshma Nanjundan on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample app creation/sample app creation/sample_app_creationApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_app_creationApp.swift
+//  sample app creation
+//
+//  Created by Reshma Nanjundan on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct sample_app_creationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
